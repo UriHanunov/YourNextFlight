871621ebc464d9ba48bffa1550be9e6527d1c28e
UriHanunov <44783443+UriHanunov@users.noreply.github.com>
2019-12-22 17:59:16 +0200

    merge

--- app/src/main/java/com/example/yournextflight/AllFlights.java
+++ app/src/main/java/com/example/yournextflight/AllFlights.java
@@ -1,31 +1,31 @@
 package com.example.yournextflight;
 
-import android.app.DatePickerDialog;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
-import android.os.Bundle;
-import android.text.TextUtils;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.DatePicker;
-import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
+        import android.app.DatePickerDialog;
+        import android.graphics.Color;
+        import android.graphics.drawable.ColorDrawable;
+        import android.os.Bundle;
+        import android.text.TextUtils;
+        import android.util.Log;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.DatePicker;
+        import android.widget.EditText;
+        import android.widget.ListView;
+        import android.widget.TextView;
+        import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
+        import androidx.annotation.NonNull;
+        import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+        import com.google.firebase.database.DataSnapshot;
+        import com.google.firebase.database.DatabaseError;
+        import com.google.firebase.database.DatabaseReference;
+        import com.google.firebase.database.FirebaseDatabase;
+        import com.google.firebase.database.ValueEventListener;
 
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.List;
+        import java.util.ArrayList;
+        import java.util.Calendar;
+        import java.util.List;
 
 public class AllFlights extends AppCompatActivity {
 
--- app/src/main/java/com/example/yournextflight/CustomerMain.java
+++ app/src/main/java/com/example/yournextflight/CustomerMain.java
@@ -5,9 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+
 public class CustomerMain extends AppCompatActivity {
 
     private TextView info;
+
+    DatabaseReference DatabaseUsers;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +21,6 @@ public class CustomerMain extends AppCompatActivity {
         setContentView(R.layout.activity_customer_main);
 
         info = (TextView)findViewById(R.id.tv2);
+        String fName = "";
     }
 }
